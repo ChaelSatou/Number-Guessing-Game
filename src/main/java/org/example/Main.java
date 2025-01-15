0dcca83898a7e38ecab9545af71a20b0c63e3be4
chaelsatou <michael.candeza11@gmail.com>
2025-01-15 16:49:30 +0800

    -added display of the top 10 scores with the username of the player.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,17 @@ package org.example;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
 
 // Author: Chael Candeza
 public class Main {
 
-    static int answers = (int)(Math.random() * 100);
+    static int answers = (int) (Math.random() * 100);
     static int attempts;
-    static int highScore = readHighScore();
-
+    static List<Score> highScores = readHighScore();
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -22,10 +24,11 @@ public class Main {
         System.out.println("""
                 1. Easy (10 chances)
                 2. Medium (5 chances)
-                3. Hard (3 chances)""");
+                3. Hard (3 chances)
+                4. Leaderboard """);
         System.out.print("Enter your choice: ");
         int choice = input.nextInt();
-        switch(choice){
+        switch (choice) {
             case 1:
                 easyLevel();
                 break;
@@ -35,100 +38,121 @@ public class Main {
             case 3:
                 hardLevel();
                 break;
+            case 4:
+                displayHighScores();
+                break;
             default:
                 System.out.println("Invalid choice try again!");
         }
         input.close();
     }
-    public static void easyLevel(){
+
+    private static void easyLevel() {
         System.out.println("Great! You have selected the Easy difficulty level");
         System.out.println("Let's start the game ");
         attempts = 10;
         //System.out.println("Random number! "+answers);
         int i = 1;
-        while ( i <=attempts ){
+        while (i <= attempts) {
             System.out.print("Enter your guess!: ");
             int easyLevelGuess = input.nextInt();
 
-            if (easyLevelGuess < answers){
-                System.out.println("Incorrect! The number is greater than "+easyLevelGuess);
+            if (easyLevelGuess < answers) {
+                System.out.println("Incorrect! The number is greater than " + easyLevelGuess);
                 i++;
-            }
-            else if (easyLevelGuess > answers) {
-                System.out.println("Incorrect! The number is less than "+easyLevelGuess);
+            } else if (easyLevelGuess > answers) {
+                System.out.println("Incorrect! The number is less than " + easyLevelGuess);
                 i++;
-            }
-            else {
-                System.out.println("Congratulations! You guessed the correct number in "+ i +" attempts.");
-                if (i < highScore || highScore == -1 ){
-                    System.out.println("New High Score!");
-                    writeHighScores(i);
+            } else {
+                System.out.println("Congratulations! You guessed the correct number in " + i + " attempts.");
+                if (highScores.size() < 10 || i < highScores.get(highScores.size() - 1).attempts) {
+                    System.out.print("You made it to the top 10! Enter your username: ");
+                    String username = input.next();
+                    highScores.add(new Score(username, i));
+                    Collections.sort(highScores);
+                    if (highScores.size() > 10) {
+                        highScores.remove(highScores.size() - 1);
+                    }
+                    writeHighScores(highScores);
                 }
                 break;
             }
-            if (i > attempts){
+            if (i > attempts) {
                 System.out.println("You have out of chances. Thanks for playing!");
             }
         }
 
     }
 
-    public static void mediumLevel(){
+    private static void mediumLevel() {
         System.out.println("Great! You have selected the Medium difficulty level");
         System.out.println("Let's start the game ");
         attempts = 5;
         //System.out.println("Random number! "+answers);
         int i = 1;
-        while ( i <=attempts ){
+        while (i <= attempts) {
             System.out.print("Enter your guess!: ");
             int mediumLevelGuess = input.nextInt();
-            
-            // I remove the static value for answers and used the math.random
-            if (mediumLevelGuess < answers){
-                System.out.println("Incorrect! The number is greater than "+mediumLevelGuess);
+
+            if (mediumLevelGuess < answers) {
+                System.out.println("Incorrect! The number is greater than " + mediumLevelGuess);
                 i++;
-            }
-            else if (mediumLevelGuess > answers) {
-                System.out.println("Incorrect! The number is less than "+mediumLevelGuess);
+            } else if (mediumLevelGuess > answers) {
+                System.out.println("Incorrect! The number is less than " + mediumLevelGuess);
                 i++;
-            }
-            else {
-                System.out.println("Congratulations! You guessed the correct number in "+ i +" attempts.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number in " + i + " attempts.");
+                if (highScores.size() < 10 || i < highScores.get(highScores.size() - 1).attempts) {
+                    System.out.print("You made it to the top 10! Enter your username: ");
+                    String username = input.next();
+                    highScores.add(new Score(username, i));
+                    Collections.sort(highScores);
+                    if (highScores.size() > 10) {
+                        highScores.remove(highScores.size() - 1);
+                    }
+                    writeHighScores(highScores);
+                }
                 break;
             }
-            if (i > attempts){
+            if (i > attempts) {
                 System.out.println("You have out of chances. Thanks for playing!");
             }
         }
 
     }
 
-    public static void hardLevel(){
+    private static void hardLevel() {
         System.out.println("Great! You have selected the Hard difficulty level");
         System.out.println("Let's start the game ");
         attempts = 3;
         //System.out.println("Random number! "+answers);
         int i = 1;
-        while ( i <=attempts ){
+        while (i <= attempts) {
             System.out.print("Enter your guess!: ");
             int hardLevelGuess = input.nextInt();
-
-            if (hardLevelGuess < answers){
-                System.out.println("Incorrect! The number is greater than "+hardLevelGuess);
+            System.out.println("Answer " + answers);
+            if (hardLevelGuess < answers) {
+                System.out.println("Incorrect! The number is greater than " + hardLevelGuess);
                 i++;
-            }
-
-            else if (hardLevelGuess > answers) {
-                System.out.println("Incorrect! The number is less than "+hardLevelGuess);
+            } else if (hardLevelGuess > answers) {
+                System.out.println("Incorrect! The number is less than " + hardLevelGuess);
                 i++;
-            }
-
-            else {
-                System.out.println("Congratulations! You guessed the correct number in "+ i +" attempts.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number in " + i + " attempts.");
+                if (highScores.size() < 10 || i < highScores.get(highScores.size() - 1).attempts) {
+                    System.out.print("You made it to the top 10! Enter your username: ");
+                    String username = input.next();
+                    highScores.add(new Score(username, i));
+                    Collections.sort(highScores);
+                    if (highScores.size() > 10) {
+                        highScores.remove(highScores.size() - 1);
+                    }
+                    writeHighScores(highScores);
+                }
                 break;
             }
 
-            if (i > attempts){
+            if (i > attempts) {
                 System.out.println("You have out of chances. Thanks for playing!");
             }
 
@@ -136,34 +160,58 @@ public class Main {
 
     }
 
-    public static int readHighScore(){
-        try{
-            File file = new File("highscore.txt");
-            if (file.exists()){
+    private static List<Score> readHighScore() {
+        List<Score> highScores = new ArrayList<>();
+        try {
+            File file = new File("highscores.txt");
+            if (file.exists()) {
                 Scanner fileReader = new Scanner(file);
-                if (fileReader.hasNextInt()){
-                    return fileReader.nextInt();
+                while (fileReader.hasNextLine()) {
+                    String line = fileReader.nextLine();
+                    String[] parts = line.split(":");
+                    highScores.add(new Score(parts[0], Integer.parseInt(parts[1].trim())));
                 }
                 fileReader.close();
             }
-        }
-        catch (IOException e) {
-            System.out.println("Error on reading the high scores.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the high scores.");
             e.printStackTrace();
         }
-        return -1;
+        return highScores;
     }
 
-    public static void writeHighScores (int score){
+    private static void writeHighScores(List<Score> highScores) {
         try {
-            FileWriter fileWriter = new FileWriter("highscore.txt");
-            fileWriter.write(Integer.toString(score));
-            fileWriter.close();
-        }
-        catch (IOException e){
-            System.out.println("Error on writing the highscore.");
+            FileWriter writer = new FileWriter("highscores.txt");
+            for (Score score : highScores) {
+                writer.write(score.username + ": " + score.attempts + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the high scores.");
             e.printStackTrace();
+        }
+    }
 
+    private static void displayHighScores() {
+        System.out.println("Top 10 High Scores:");
+        for (Score score : highScores) {
+            System.out.println(score.username + ": " + score.attempts + " attempts");
+        }
+    }
+
+    private static class Score implements Comparable<Score> {
+        String username;
+        int attempts;
+
+        Score(String username, int attempts) {
+            this.username = username;
+            this.attempts = attempts;
+        }
+
+        @Override
+        public int compareTo(Score other) {
+            return Integer.compare(this.attempts, other.attempts);
         }
     }
 }
